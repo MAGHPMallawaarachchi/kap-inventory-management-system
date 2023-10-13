38fe0bb56defdc66e3f68113a34b2a42e5e4be18
MKPNJoanne <97240800+MKPNJoanne@users.noreply.github.com>
2023-10-13 19:49:30 +0530

    chore: add CustomerModel

---
+++ Models/CustomerModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory_management_system_kap.Models
+{
+    public class CustomerModel
+    {
+        private string customerId;
+        private string name;
+        private string address;
+        private string city;
+        private int contactNo;
+
+        public string CustomerId { get => customerId; set => customerId = value; }
+        public string Name { get => name; set => name = value; }
+        public string Address { get => address; set => address = value; }
+        public string City { get => city; set => city = value; }
+        public int ContactNo { get => contactNo; set => contactNo = value; }
+    }
+}
