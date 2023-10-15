bcff538fb4c37c483fe1fa7493c6a63bf1d2f21e
Hasini Punsala <hasinipunsala@gmail.com>
2023-10-15 22:58:53 +0530

    feat: get all supplier ids
    Co-Authored-By: Manthi Seneka Gamage <97454472+tgmseneka@users.noreply.github.com>

---
+++ Controllers/SupplierController.cs
@@ -0,0 +1,24 @@
+﻿using inventory_management_system_kap.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory_management_system_kap.Controllers
+{
+    public class SupplierController
+    {
+        private readonly SupplierRepository _supplierRepository;
+
+        public SupplierController(SupplierRepository repository)
+        {
+            this._supplierRepository = repository;
+        }
+
+        public IEnumerable<string> GetAllSupplierIds()
+        {
+            return _supplierRepository.GetAllSupplierIds();
+        }
+    }
+}
---
+++ Repositories/SupplierRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory_management_system_kap.Repositories
+{
+    public class SupplierRepository
+    {
+        private readonly string connectionString;
+
+        public SupplierRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public IEnumerable<string> GetAllSupplierIds()
+        {
+            string query = "SELECT SupplierId FROM Supplier";
+
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                using (var reader = command.ExecuteReader())
+                {
+                    var suppliers = new List<string>();
+                    while (reader.Read())
+                    {
+                        suppliers.Add(reader["SupplierId"].ToString());
+                    }
+                    return suppliers;
+                }
+            }
+        }
+    }
+}
