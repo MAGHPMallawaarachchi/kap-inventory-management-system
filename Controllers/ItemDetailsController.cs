e2ed3601ce741a3d608b9a4e0a21a9478c0a16a5
Chamith Dilshan <ssoftware190@gmail.com>
2023-10-16 23:49:47 +0530

    Add repository class for itemdetails

---
+++ Controllers/ItemDetailsController.cs
@@ -0,0 +1,33 @@
+﻿using inventory_management_system_kap.Models;
+using inventory_management_system_kap.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory_management_system_kap.Controllers
+{
+    public class ItemDetailsController { 
+        
+        private readonly ItemDetailsRepository _itemDetailsRepository;
+
+        public ItemDetailsController(ItemDetailsRepository itemDetailsepository)
+        {
+            _itemDetailsRepository = itemDetailsepository;
+        }
+
+        public ItemDetailsModel GetItemDetailsByPartNo(string partNo)
+        {
+            IEnumerable<ItemDetailsModel> itemDetailsList = _itemDetailsRepository.GetItemDetailsByPartNo(partNo);
+
+            if (itemDetailsList != null && itemDetailsList.Any())
+            {
+                return itemDetailsList.First();
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ Repositories/ItemDetailsRepository.cs
@@ -0,0 +1,66 @@
+﻿using inventory_management_system_kap.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static inventory_management_system_kap.Models.ItemDetailsModel;
+
+namespace inventory_management_system_kap.Repositories
+{
+    public class ItemDetailsRepository
+    {
+        private readonly string connectionString;
+
+        public ItemDetailsRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        //public ItemModel GetItemDetailsByPartNo(string partNo)
+        public IEnumerable<ItemDetailsModel> GetItemDetailsByPartNo(string partNo)
+        {
+            string query = "SELECT * FROM Item WHERE PartNo = @PartNo";
+            var itemDetailsList = new List<ItemDetailsModel>();
+            var parameters = new Dictionary<string, object>
+            {
+                { "@PartNo", partNo }
+            };
+
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand(query, connection))
+            {
+                foreach (var param in parameters)
+                {
+                    command.Parameters.Add(new SqlParameter(param.Key, param.Value));
+                }
+
+                connection.Open();
+
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        var itemDetailsModel = new ItemDetailsModel
+                        {
+                            PartNo = (string)reader["PartNo"],
+                            OEMNo = (string)reader["OEMNo"],
+                            BrandId = (string)reader["BrandId"],
+                            QtySold = (int)reader["QtySold"],
+                            QtyInHand = (int)reader["QtyInHand"],
+                            TotalQty = (int)reader["TotalQty"],
+                            Category = (string)reader["category"],
+                            Description = (string)reader["description"],
+                            BuyingPrice = (decimal)reader["BuyingPrice"],
+                            UnitPrice = (decimal)reader["UnitPrice"],
+                            // Add other properties as needed
+                        };
+                        itemDetailsList.Add(itemDetailsModel);
+                    }
+                }
+            }
+            return itemDetailsList;
+        }
+    }
+}
