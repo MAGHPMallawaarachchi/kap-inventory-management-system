38c0cf7e6677e8f14149cb605072f4c3901779f7
Chamith Dilshan <ssoftware190@gmail.com>
2023-10-16 23:48:27 +0530

    Add model class for itemetails

---
+++ Models/ItemDetailsModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory_management_system_kap.Models
+{
+    public class ItemDetailsModel {
+
+            private string partNo;
+            private string oemNo;
+            private string brandId;
+            private int qtySold;
+            private int qtyInHand;
+            private int totalQty;
+            private string category;
+            private string description;
+            private decimal buyingPrice;
+            private decimal unitPrice;
+            private string country;
+            private string name;
+            private string email;
+            //need to added the itemImage
+
+            public string PartNo { get => partNo; set => partNo = value; }
+            public string OEMNo { get => oemNo; set => oemNo = value; }
+            public string BrandId { get => brandId; set => brandId = value; }
+            public int QtySold { get => qtySold; set => qtySold = value; }
+            public int QtyInHand { get => qtyInHand; set => qtyInHand = value; }
+            public int TotalQty { get => totalQty; set => totalQty = value; }
+            public string Category { get => category; set => category = value; }
+            public string Description { get => description; set => description = value; }
+            public decimal BuyingPrice { get => buyingPrice; set => buyingPrice = value; }
+            public decimal UnitPrice { get => unitPrice; set => unitPrice = value; }
+            public string Country { get => partNo; set => partNo = value; }
+            public string Name { get => oemNo; set => oemNo = value; }
+            public string Email { get => brandId; set => brandId = value; }
+
+    }
+}
